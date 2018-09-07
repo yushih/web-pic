38fdd29c9de5d91c991bbbba1e5602430d5c8ed5
yushi <yushihuang@gmail.com>
2018-09-07 17:58:30 +0800

    embed intro.html into page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import SplitPane from 'react-split-pane';
+import intro from './intro.html';
+
 
 class App extends Component {
   render() {
     return (
         <SplitPane split='vertical' defaultSize={300}>
             <SplitPane split='horizontal' defaultSize={200}>
-                <div>intro</div>
+                <div dangerouslySetInnerHTML={{__html:intro}}></div>
                 <div>code</div>
             </SplitPane>
             <div className='graph'>graph</div>
